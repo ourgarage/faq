0926c2d356bd59599e5437d4cff7ce204840321e
Egor Zhelobanov <e.zhelobanov@gmail.com>
2016-12-20 17:48:27 +1100

    pr

---
+++ src/FaqServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ourgarage\Faq;
+
+use Illuminate\Support\ServiceProvider;
+
+class FaqServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        require __DIR__ . '/routes/web.php';
+
+        /*$this->loadViewsFrom(__DIR__.'/resources/views', 'faq');
+
+        $this->loadTranslationsFrom(__DIR__.'/resources/lang', 'faq');
+
+        $this->publishes([
+            __DIR__.'/resources/assets' => public_path('packages/faq'),
+        ], 'price');
+
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');*/
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->make('Ourgarage\Faq\Http\Controllers\Admin\FaqController');
+
+        $this->mergeConfigFrom(__DIR__.'/config/faq.php', 'packages');
+    }
+}
