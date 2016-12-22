127ac4f0cf3d09e9628eff5ddacc4025fff63073
Egor Zhelobanov <e.zhelobanov@gmail.com>
2016-12-22 16:47:27 +1100

    QA

--- src/Http/Controllers/Admin/FaqQAController.php
+++ src/Http/Controllers/Admin/FaqQAController.php
@@ -3,8 +3,13 @@
 namespace Ourgarage\Faq\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Ourgarage\Faq\Presenters\FaqPresenter;
+use Notifications;
 
 class FaqQAController extends Controller
 {
+    public function index(FaqPresenter $presenter)
+    {
 
+    }
 }
