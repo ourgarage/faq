a389729fcb34a9dbae969cc03d5f964dcc8c56d4
Egor Zhelobanov <e.zhelobanov@gmail.com>
2017-01-19 17:58:46 +1100

    In process...

--- src/resources/views/site/index.blade.php
+++ src/resources/views/site/index.blade.php
@@ -12,33 +12,20 @@
                         @foreach($categories as $category)
                             @if(!$category->questionsAnswers->isEmpty())
                                 <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        <h4 class="panel-title">
-                                            <a data-toggle="collapse" data-parent="#accordion"
-                                               href="#collapse{{ $category->id }}">
-                                                {{ $category->title }}
-                                            </a>
-                                        </h4>
-                                    </div>
-                                    <div id="collapse{{ $category->id }}" class="panel-collapse collapse">
-                                        <div class="panel-body">
-
-                                        </div>
-                                    </div>
-                                </div>
-
-
-                                <div class="panel panel-default">
                                     <div class="panel-heading" role="tab" id="headingOne">
                                         <h4 class="panel-title">
-                                            <a role="button" data-toggle="collapse" data-parent="#accordion"
-                                               href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                            <a role="button" data-toggle="collapse"
+                                               data-parent="#accordion"
+                                               href="#collapse{{ $category->id }}"
+                                               aria-expanded="true"
+                                               aria-controls="collapse{{ $category->id }}">
                                                 {{ $category->title }}
                                             </a>
                                         </h4>
                                     </div>
-                                    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel"
-                                         aria-labelledby="headingOne">
+                                    <div id="collapse{{ $category->id }}" class="panel-collapse collapse"
+                                         role="tabpanel"
+                                         aria-labelledby="heading{{ $category->id }}">
                                         <div class="panel-body">
                                             @foreach($category->questionsAnswers as $questionAnswer)
                                                 <ul>
