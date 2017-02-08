4b8d0aaaa78f2d2b41b2103b149991d65b56fb3f
Egor Zhelobanov <e.zhelobanov@gmail.com>
2017-02-08 17:48:43 +1100

    Filter start...

--- src/resources/views/site/index.blade.php
+++ src/resources/views/site/index.blade.php
@@ -8,6 +8,21 @@
             </div>
             <div class="panel-body">
                 @if(!$categories->isEmpty())
+
+                    <form id="main" v-cloak>
+
+                        <div class="bar">
+                            <input type="text" v-model="searchString" placeholder="Enter your search terms"/>
+                        </div>
+
+                        <ul>
+                            <li v-for="i in articles | searchFor searchString">
+                                <a v-bind:href="i.url"><img v-bind:src="i.image"/></a>
+                                <p>@{{i.title}}</p>
+                            </li>
+                        </ul>
+                    </form>
+
                     @foreach($categories as $category)
                         @if(!$category->faq->isEmpty())
                             <div class="panel panel-default">
@@ -18,10 +33,18 @@
                             </div>
                         @endif
                     @endforeach
+
                 @else
                     {{ trans('faq::faq.front.no-qa') }}
                 @endif
             </div>
         </div>
     </div>
+@endsection
+
+@section('js')
+    <script src='/libs/jquery/jquery.min.js'></script>
+    <script src='/libs/bootstrap/js/bootstrap.min.js'></script>
+    <script src="/libs/vue/vue.min.js"></script>
+    <script src="/packages/faq/js/filter.js"></script>
 @endsection
