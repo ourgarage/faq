4596a8ec857a098797998c356ef9b944cbf7564c
Egor Zhelobanov <e.zhelobanov@gmail.com>
2017-02-09 11:18:05 +0000

    Fixed

--- src/database/migrations/2016_12_20_123045_create_faq_questions_table.php
+++ src/database/migrations/2016_12_20_123045_create_faq_questions_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-use Ourgarage\Faq\Models\QuestionAnswer;
+use Ourgarage\Faq\Models\Faq;
 
 class CreateFaqQuestionsTable extends Migration
 {
@@ -17,7 +17,7 @@ class CreateFaqQuestionsTable extends Migration
         Schema::create('faq_questions', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('faq_category_id')->unsigned();
-            $table->string('status')->default(QuestionAnswer::STATUS_ACTIVE)->index();
+            $table->string('status')->default(Faq::STATUS_ACTIVE)->index();
             $table->string('title')->unique();
             $table->string('slug')->unique();
             $table->text('answer');
